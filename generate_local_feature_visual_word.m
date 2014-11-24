bdd31d0d17b79f499ab6b36e78e2bede5e5208ef
PC <852426419@qq.com>
2014-11-24 20:23:14 +0800

    commit

--- generate_local_feature_visual_word.m
+++ generate_local_feature_visual_word.m
@@ -1,27 +1,55 @@
-%
-%文件功能：对经过calssify_features.m处理后的特征进行聚类（需要聚类128*128次）
-%输入参数：分类后的特征数据，一个txt文件，里面有所有特征的id。
-%输出参数：得到128*128个词典，一个位置对应一个_.v文件
+%%
+%文件功能：对经过local feture提取后的特征进行聚类
+%输入参数：所有的local feature
+%输出参数：聚类中心(即词典)
 %
 
-features_list = textread('test.txt', '%s'); %classify_data_list.txt 分类后的特征数据列表
+%%
+features_list = textread('local_feature_full.list', '%s'); % local feature列表
 len = size(features_list);
 len = len(1);
 fprintf('len %d\n', len);
-for n = 1:len %循环处理每个文件
-    featurePath = features_list{n};
-    fprintf('%d processing %s\n', n, featurePath);
-    fid = fopen(featurePath,'r');
-    I = fread(fid,'single');
-    fclose(fid);
-    I_2 = reshape(I,36,[]);
-    I_3 = I_2';
-    [IDX,C] = kmeans(I_3,200,'MaxIter',100); % 对特征进行kmeans聚类，聚类中心个数有实验效果确定（I_3为输入矩阵，50为聚类中心个数，100为最大的迭代次数）
-    
-    D = C';
-    vocabulary = reshape(D,1,[]);
-    [filethstr, name, ext] = fileparts(featurePath);
-    fid_f = fopen(fullfile('test',strcat(name, '._v')), 'w'); %聚类结果（即词典）写入_.v文件中
-    fwrite(fid_f, vocabulary, 'single');
-    fclose(fid_f);
+
+feature = zeros(10000000,36);   %存储所有local feature的矩阵
+zero_feature = zeros(1,36);
+
+fprintf('collecting feature');
+
+count = 1;
+for n = 1:len   % 读入所有local feature文件
+    if count~= 10000000
+        featurePath = features_list{n};
+        %         fprintf('%d processing %s\n', n, featurePath);
+        local_feature = load(featurePath,'hog_feature');
+        hog_feature = local_feature.hog_feature;
+        for i=1:size(hog_feature,1)
+            if hog_feature(i,:) ~= zero_feature
+                feature(count,:) = hot_feature(i,:);
+                count = count + 1;
+            end
+        end
+        clear local_feature;
+        clear hog_feature;
+    end
 end
+
+fprintf('finished collecting feature');
+%%  聚类得到词典
+
+% idx = kmeans(X,k),partition the observations of the n-by-p data matrix X into k clusters,
+% and returns an n-by-1 vector (idx) containing cluster indices of each observation.
+% Rows of X correspond to points and columns correspond to variables.
+% By default, kmeans uses the squared Euclidean distance measure and the k-means++ algorithm for cluster center initialization.
+% 矩阵X的每一行代表一个点，每一列代表每个点的变量
+%
+[IDX,C] = kmeans(feature,200,'MaxIter',100); % 对特征进行kmeans聚类，聚类中心个数有实验效果确定（I_3为输入矩阵，50为聚类中心个数，100为最大的迭代次数）
+save()
+    str = strcat('visual vocabulary/',name);
+    filename = strcat(str,'_local');
+    save(filename,'hog_feature');   %   保存local feature
+
+
+
+
+
+
