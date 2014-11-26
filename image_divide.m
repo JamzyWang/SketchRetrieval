99b784304272cb0d298ab818bd81640ba5988e75
notebook <wcmj1023@gmail.com>
2014-11-26 14:09:15 +0800

    commit

--- image_divide.m
+++ image_divide.m
@@ -8,7 +8,7 @@ image_percent = 0.2;    % cell中非零元素占上一层cell中非零元素的比例
 cell_percent = 0.2;     % cell中非零元素占当前层cell元素总数的比例
 
 %%  读取图像列表文件
-images_list = textread('edge_full.list', '%s');
+images_list = textread('edge_full_list.txt', '%s');
 len = size(images_list);
 len = len(1);
 fprintf('len %d\n', len);
@@ -16,7 +16,7 @@ fprintf('len %d\n', len);
 %%  对每一个图像进行分割处理
 for i = 1:len
   imgPath = images_list{i};
-  fprintf('%d start to divide %s\n', i, imgPath);
+  fprintf('%d processing %s\n', i, imgPath);
   
   image = load(imgPath,'a');    % 读取图像经过边缘检测后的数据（a为256*256的矩阵）
   [D,D1,D2,D3,D4,D5] = divide_function( image.a,cell_percent,image_percent);
@@ -25,6 +25,5 @@ for i = 1:len
   str = strcat('image_matrix_after_divide/',name);
   filename = strcat(str,'_divide');
   save(filename,'D','D1','D2','D3','D4','D5');   %   保存分割矩阵
-  fprintf('%d finished %s\n', i, imgPath);
 end
 %% quit;
